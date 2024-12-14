a058643094803eea604dd1d41748d79c4f726c98
Holly <162161116+Holl4444@users.noreply.github.com>
2024-12-14 23:20:34 +0000

    Add uk current temp. Change text Cloudy
    Changed Cloudy html text to current details as
    this element will show whatever the current condition is.
    
    Moved chance to rain to Today to reflect that it covers
    24 hours.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Display current temp in current temp para
 const dkCurrentTemp = document.getElementById('dk-ct');
+const ukCurrentTemp = document.getElementById('uk-ct');
 
-//get element and add textContent
-
-async function fetchData(getCurrentTemp) {
+async function fetchData(displayCurrentTemp) {
   try {
     const response = await fetch(
       'https://api.open-meteo.com/v1/forecast?latitude=56.4532,52.1534&longitude=9.402,-0.702&current=temperature_2m,is_day,precipitation,cloud_cover,wind_speed_10m,wind_gusts_10m&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto',
@@ -17,10 +15,12 @@ async function fetchData(getCurrentTemp) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    // Handle your data
-    getCurrentTemp(data[0].current.temperature_2m);
-    console.log(data[1]);
-
+    // Handle your datadisplay(data[0].current.temperature_2m);
+    displayCurrentTemp(
+      data[0].current.temperature_2m,
+      data[1].current.temperature_2m
+    );
+    console.log(data);
     return data;
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -28,8 +28,13 @@ async function fetchData(getCurrentTemp) {
   }
 }
 
-function getCurrentTemp(temp) {
-  dkCurrentTemp.textContent = `Current Temp: ${temp}`;
+function displayCurrentTemp(dktemp, uktemp) {
+  dkCurrentTemp.textContent = `Current Temp: ${dktemp}`;
+  ukCurrentTemp.textContent = `Current Temp: ${uktemp}`;
 }
 
-fetchData(getCurrentTemp);
+// function getCurrentCloudCover(clouds) {
+//   dkCurren;
+// }
+
+fetchData(displayCurrentTemp);
